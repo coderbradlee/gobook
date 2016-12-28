8017ac0d8bd910e791555dfc873ba7c239167a28
root <root@>
2016-12-28 20:08:24 +0800

    pipeline

--- client/client.go
+++ client/client.go
@@ -10,9 +10,16 @@ func main(){
 	if err!=nil{
 		log.Fatal(err)
 	}
-	defer conn.Close()
-	go mustCopy(os.Stdout,conn)
+	done:=make(chan struct{})
+	go func(){
+		io.Copy(os.Stdout,conn)
+		log.Println("done")
+		done<-struct{}{}
+	}()
+
 	mustCopy(conn,os.Stdin)
+	conn.Close()
+	<-done
 }
 func mustCopy(dst io.Writer,src io.Reader){
 	if _,err:=io.Copy(dst,src);err!=nil{
