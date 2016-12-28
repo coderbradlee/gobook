a632ef1a53a39f5b5db6210351233d8201bc3935
lzxm160 <lzxm160@sina.com>
2016-12-28 20:15:01 +0800

    pipeline func

--- pipeline/main.go
+++ pipeline/main.go
@@ -1,24 +1,43 @@
 package main
 
 import "fmt"
-
+func counter(out chan<-int){
+	for x:=0;x<5;x++{
+		out<-x
+	}
+	// close(out)
+}
+func squarer(out chan<-int,in <-chan int){
+	for x:=range in{
+		out<-x*x
+	}
+	// close(out)
+}
+func printer(in <-chan int) {
+	for v:=in{
+		fmt.Println(v)
+	}
+}
 func main() {
 	naturals:=make(chan int)
 	squares:=make(chan int)
-	go func(){
-		for x:=0;x<5;x++{
-			naturals<-x
-		}
-		close(naturals)
-	}()
-	go func(){
-		for x:=range naturals{
-			squares<-x*x
-		}
-		close(squares)
-	}()
-	for x:=range squares{
-		fmt.Println(x)
-	}
-	fmt.Println("vim-go")
+	go counter(naturals)
+	go squarer(squares,naturals)
+	printer(squares)
+	// go func(){
+	// 	for x:=0;x<5;x++{
+	// 		naturals<-x
+	// 	}
+	// 	close(naturals)
+	// }()
+	// go func(){
+	// 	for x:=range naturals{
+	// 		squares<-x*x
+	// 	}
+	// 	close(squares)
+	// }()
+	// for x:=range squares{
+	// 	fmt.Println(x)
+	// }
+	// fmt.Println("vim-go")
 }
