554cb79f7412cf44231ac176237186548874068f
lzxm160 <lzxm160@sina.com>
2016-12-28 21:10:30 +0800

    pipeline func

--- pipeline/main.go
+++ pipeline/main.go
@@ -28,14 +28,46 @@ func mirroredQuery()string {
 func request(hostname string)(response string){
 	return hostname
 }
+var tokens=make(chan struct{},200)
+func crawl(url string)[]string{
+	fmt.Println(url)
+	tokens<-struct{}{}
+	list,err:=links.Extract(url)
+	<-tokens
+	if err!=nil{
+		log.Print(err)
+	}
+	return list
+}
+func startCrawl() {
+	worklist:=make(chan[]string)
+	var n int
+	n++
+	go func(){worklist<-"baidu.com"}()
+	seen:=make(map[string]bool)
+	for ;n>0;n--{
+		list:=<-worklist
+		for _,link:=range list{
+			if!seen[link]{
+				seen[link]=true
+				n++
+				go func(link string) {
+					worklist<-crawl(link)
+				}(link)
+			}
+
+		}
+	}
+}
 func main() {
+	startCrawl()
 	// naturals:=make(chan int)
 	// squares:=make(chan int)
 	// go counter(naturals)
 	// go squarer(squares,naturals)
 	// printer(squares)
-	ret:=mirroredQuery()
-	fmt.Println(ret)
+	// ret:=mirroredQuery()
+	// fmt.Println(ret)
 	// go func(){
 	// 	for x:=0;x<5;x++{
 	// 		naturals<-x
