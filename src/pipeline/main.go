d986f365b8a0b90864afba371f9e5cf4155f3aa2
lzxm160 <lzxm160@sina.com>
2016-12-28 22:12:02 +0800

    dir

--- src/pipeline/main.go
+++ src/pipeline/main.go
@@ -2,33 +2,7 @@ package main
 
 import "fmt"
 import "links"
-func counter(out chan<-int){
-	for x:=0;x<5;x++{
-		out<-x
-	}
-	close(out)
-}
-func squarer(out chan<-int,in <-chan int){
-	for x:=range in{
-		out<-x*x
-	}
-	close(out)
-}
-func printer(in <-chan int) {
-	for v:=range in{
-		fmt.Println(v)
-	}
-}
-func mirroredQuery()string {
-	responses:=make(chan string,3)
-	go func(){responses<-request("1")}()
-	go func(){responses<-request("2")}()
-	go func(){responses<-request("3")}()
-	return <-responses
-}
-func request(hostname string)(response string){
-	return hostname
-}
+
 var tokens=make(chan struct{},200)
 func crawl(url string)[]string{
 	fmt.Println(url)
