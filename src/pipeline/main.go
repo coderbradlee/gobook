c7fe15c3c6dbf727a22b27c9e9f7230c610e9c1b
lzxm160 <lzxm160@sina.com>
2017-01-02 15:58:31 +0800

    symbol

--- src/pipeline/main.go
+++ src/pipeline/main.go
@@ -58,7 +58,7 @@ func main() {
 	c1:=sha256.Sum256([]byte("x"))
 	c2:=sha256.Sum256([]byte("X"))
 	fmt.Printf("%x\n%x\n%t\n%T\n",c1,c2,c1==c2,c1)
-	fmt.Printf("%T %T\n",a,d)
+	fmt.Printf("%T %T %T\n",a,b,d)
 	// startCrawl()
 	// naturals:=make(chan int)
 	// squares:=make(chan int)
